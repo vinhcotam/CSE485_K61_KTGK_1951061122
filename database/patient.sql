400099cdbeb7c614494b5eef967d3bb345e4c9ed
vinh4501 <vinhcotam04052001@gmail.com>
2021-10-21 13:29:08 +0700

    update conf and db

--- database/patient.sql
+++ database/patient.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 21, 2021 lúc 08:19 AM
+-- Thời gian đã tạo: Th10 21, 2021 lúc 08:28 AM
 -- Phiên bản máy phục vụ: 10.4.19-MariaDB
 -- Phiên bản PHP: 8.0.7
 
@@ -28,16 +28,16 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tb_patient` (
-  `patientid` int(10) UNSIGNED NOT NULL,
-  `firstname` varchar(100) DEFAULT NULL,
-  `lastname` varchar(100) DEFAULT NULL,
+  `patientid` int(11) NOT NULL,
+  `firstname` varchar(15) NOT NULL,
+  `lastname` varchar(15) NOT NULL,
   `dateofbirth` date DEFAULT NULL,
-  `gender` varchar(100) DEFAULT NULL,
-  `mobile` varchar(100) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `height` int(11) DEFAULT NULL,
   `weight` int(11) DEFAULT NULL,
-  `blood_type` varchar(100) DEFAULT NULL,
+  `blood_type` varchar(10) DEFAULT NULL,
   `created_on` date DEFAULT NULL,
   `modified_on` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -47,8 +47,28 @@ CREATE TABLE `tb_patient` (
 --
 
 INSERT INTO `tb_patient` (`patientid`, `firstname`, `lastname`, `dateofbirth`, `gender`, `mobile`, `email`, `height`, `weight`, `blood_type`, `created_on`, `modified_on`) VALUES
-(0, 'Ho', 'Vinh', '2001-05-04', 'nam', '0966175932', 'vinhcotam04052001@gmail.com', 180, 61, 'O', '0000-00-00', '0000-00-00'),
-(0, 'Lê', 'Linh', '2001-10-04', 'nu', '0966175933', 'linhlee@gmail.com', 160, 41, 'A', '2021-10-10', '2021-12-10');
+(1, 'Ho', 'Vinh', '2001-05-04', 'nam', '0966175932', 'vinhcotam04052001@gmail.com', 180, 61, 'O', '0000-00-00', '0000-00-00'),
+(2, 'Lê', 'Linh', '2001-10-04', 'nu', '0966175933', 'linhlee@gmail.com', 160, 41, 'A', '2021-10-10', '2021-12-10');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `tb_patient`
+--
+ALTER TABLE `tb_patient`
+  ADD PRIMARY KEY (`patientid`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `tb_patient`
+--
+ALTER TABLE `tb_patient`
+  MODIFY `patientid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
