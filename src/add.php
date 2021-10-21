dd6ce6268440c5903d875cb1b5e8ff13c55480e5
vinh4501 <vinhcotam04052001@gmail.com>
2021-10-21 15:09:42 +0700

    sửa add

--- src/add.php
+++ src/add.php
@@ -35,6 +35,18 @@
                 </div>
             </div>
             <div class="form-group row">
+                <label for="dateofbirth" class="col-sm-2 col-form-label">Ngày sinh</label>
+                <div class="col-sm-10">
+                    <input type="date" class="form-control" id="dateofbirth" name="dateofbirth">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="gender" class="col-sm-2 col-form-label">giới tính</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="gender" name="gender">
+                </div>
+            </div>
+            <div class="form-group row">
                 <label for="mobile" class="col-sm-2 col-form-label">Số di động</label>
                 <div class="col-sm-10">
                     <input type="tel" class="form-control" id="mobile" name="mobile">
@@ -46,7 +58,36 @@
                     <input type="email" class="form-control" id="email" name="email">
                 </div>
             </div>
-
+            <div class="form-group row">
+                <label for="height" class="col-sm-2 col-form-label">Chiều cao</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="height" name="height">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="weight" class="col-sm-2 col-form-label">Cân nặng</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="weight" name="weight">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="blood_type" class="col-sm-2 col-form-label">Nhóm máu</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="blood_type" name="blood_type">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="created_on" class="col-sm-2 col-form-label">Ngày lập sổ</label>
+                <div class="col-sm-10">
+                    <input type="date" class="form-control" id="created_on" name="created_on">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="modified_on" class="col-sm-2 col-form-label">Ngày cập nhật</label>
+                <div class="col-sm-10">
+                    <input type="date" class="form-control" id="modified_on" name="modified_on">
+                </div>
+            </div>
             <div class="form-group row">
                 <label for="empMobile" class="col-sm-2 col-form-label"></label>
                 <div class="col-sm-10">
--- src/process_add.php
+++ src/process_add.php
@@ -1,17 +1,25 @@
 <?php
     $firstname =$_POST['firstname'];
     $lastname =$_POST['lastname'];
+    $birthofdate =$_POST['birthofdate'];
+    $gender =$_POST['gender'];
     $mobile =$_POST['mobile'];
     $email =$_POST['email'];
+    $height =$_POST['height'];
+    $weight =$_POST['weight'];
+    $blood_type =$_POST['blood_type'];
+    $created_on =$_POST['created_on'];
+    $modified_on =$_POST['modified_on'];
     include 'conf.php';
-    $sql = "INSERT INTO tb_patient (firstname,lastname, mobile, email)
-    VALUES ('$firstname','$lastname','$mobile','$email')";
+    $sql = "INSERT INTO tb_patient (firstname,lastname,dateofbirth,gender ,mobile, email,height,weight,blood_type,created_on,modified_on)
+    VALUES ('$firstname','$lastname','$birthofdate','$gender','$mobile','$email','$height','$weight','$blood_type','$created_on','$modified_on')";
     echo $sql;
     $result=mysqli_query($conn,$sql);
     if($result>0){
         header("Location: index.php");
     }else{
         echo "Loi";
+        header("Location: error.php");
     }
     mysqli_close($conn);
 ?>
